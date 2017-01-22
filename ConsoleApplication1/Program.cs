e2af96bf107022ac0f4e1fb95686240a36062cc7
陈杏 <@F5-IT>
2017-01-22 16:13:20 +0800

    添加项目文件。

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("有你没你都一样！\n");
+            Console.WriteLine("学而时习之，不亦乐乎!");
+            Console.ReadLine();
+        }
+    }
+}
